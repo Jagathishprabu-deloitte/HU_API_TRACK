b4de817c9eaf088bea666f7ea96e02f30042c7c4
S <jagats@deloitte.com>
2022-03-31 13:24:35 +0530

    Rest Assured Mini Assignment 1 updated

--- RestAssured Mini Assignment 1/src/test/java/RestAssured.java
+++ RestAssured Mini Assignment 1/src/test/java/RestAssured.java
@@ -1,28 +1,34 @@
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
-
 import java.io.File;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 public class RestAssured {
 
     @Test
     public void get_test_call(){
+        Response response =
         given().
                 baseUri("https://jsonplaceholder.typicode.com").
-                header("Content-Type","application/json").
         when().
                 get("/posts").
-        then().
-                statusCode(200).
-                body("id[39]",equalTo(40)).
-                body("userId[39]",equalTo(4)).
-                body(containsString("title"));
+        then().extract().response();
+        assert (response.getStatusCode() == 200);
+        assert (response.getContentType().contains("json"));
+        JSONArray arr = new JSONArray(response.asString());
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = arr.getJSONObject(i);
+            if(obj.getInt("id")==40){
+                assert(obj.getInt("userId")==4);
+            }
+            assertThat(obj.getString("title"),true);
+        }
     }
 
     @Test
